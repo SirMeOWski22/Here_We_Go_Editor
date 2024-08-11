3399a84efca3757dc6d5cce1f31283d85be4c082
Morgan <amorgan680@gmail.com>
2024-08-11 12:20:47 -0500

    Debugged the dist folder and corrected paths

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const path = require('path'); // Add this line to require the path module
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../client/dist'));
+// Use path.join to correctly resolve the path to the dist directory
+app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Serve index.html for all routes that are not API routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
+});
 
 require('./routes/htmlRoutes')(app);
 
